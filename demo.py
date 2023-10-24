83b5f59173f776cbb784bd322c95ef798dfa90f7
anzifeng <zifeng.allen@gmail.com>
2023-10-23 20:07:05 -0700

    add requirements.txt

--- demo.py
+++ demo.py
@@ -10,15 +10,16 @@ args = parser.parse_args()
 
 # Initialize environment
 go_env = gym.make('gym_go:go-v0', size=args.boardsize, komi=args.komi)
-
+go_env.reset()
 # Game loop
 done = False
 while not done:
-    action = go_env.render(mode="human")
+
+    action = go_env.render(mode="terminal")
     state, reward, done, info = go_env.step(action)
 
     if go_env.game_ended():
         break
     action = go_env.uniform_random_action()
     state, reward, done, info = go_env.step(action)
-go_env.render(mode="human")
+go_env.render(mode="terminal")
